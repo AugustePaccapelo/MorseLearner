19187d7bda121728be0acd62a97e0df86ec7bb11
Paccapelo Auguste <a.paccapelo@student.isartdigital.com>
2025-04-29 09:10:02 +0200

    Added morse alphabet
    - International Morse code stored in the dictionnary "alphabet" in MorseCode

--- Project/Scripts/Utils/MorseCode.cs
+++ Project/Scripts/Utils/MorseCode.cs
@@ -1,5 +1,6 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 // Author : Auguste Paccapelo
 
@@ -9,8 +10,20 @@ namespace Com.IsartDigital.OBG.Utils
 	{
 		public const string DOT_CHARAC = ".";
 		public const string DASH_CHARAC = "-";
-		public const string LITTLE_SPACE_CHARAC = " ";
-		public const string LETTER_SPACE_CHARAC = "   ";
-		public const string WORD_SPACE_CHARAC = "     ";
+		public const string LITTLE_SPACE_CHARAC = "";
+		public const string LETTER_SPACE_CHARAC = " ";
+		public const string WORD_SPACE_CHARAC = "   ";
+
+		public static Dictionary<String, String> alphabet = new Dictionary<string, string>()
+		{
+			{"a", ".-" }, {"b", "-..." }, {"c", "-.-." }, {"d", "-.." }, {"e", "."},
+            {"f", "..-." }, {"g", "--." }, {"h", "...." }, {"i", ".." }, {"j", ".---"},
+            {"k", "-.-" }, {"l", ".-.." }, {"m", "--" }, {"n", "-." }, {"o", "---"},
+            {"p", ".--." }, {"q", "--.-" }, {"r", ".-." }, {"s", "..." }, {"t", "-"},
+            {"u", "..-" }, {"v", "...-" }, {"w", ".--" }, {"x", "-..-" }, {"y", "-.--"},
+            {"z", "--.." }, {"0", "-----" }, {"1", ".----" }, {"2", "..---" }, {"3", "...--" },
+			{"4", "....-"}, {"5", "....." }, {"6", "-...." }, {"7", "--..." }, {"8", "---.." },
+			{"9", "----."}
+        };
 	}
 }
